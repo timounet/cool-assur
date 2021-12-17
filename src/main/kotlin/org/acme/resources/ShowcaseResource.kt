f3e2fc3d086c850236381806f64827d77d448f13
PERDRIAU Timothee <timothee.perdriau@allianz.fr>
2021-12-17 16:50:44 +0100

    try to add version in tag name

--- src/main/kotlin/org/acme/resources/ShowcaseResource.kt
+++ src/main/kotlin/org/acme/resources/ShowcaseResource.kt
@@ -1,11 +1,13 @@
 package org.acme.resources;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
 import org.eclipse.microprofile.openapi.annotations.media.Content
 import org.eclipse.microprofile.openapi.annotations.media.Schema
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
+import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -15,6 +17,11 @@ import javax.ws.rs.core.Response
 @Tag(name = "z Autres demonstration", description = "une resource pour montrer d'autres comportements")
 @Path("/v1/showcase")
 class ShowcaseResource {
+
+    @Inject
+    @field: ConfigProperty(name = "quarkus.application.version", defaultValue = "defaultVersion")
+    lateinit var version: String
+
     @GET
     @Path("/randomError")
     @Operation(summary = "Génere parfois une erreur aléatoire")
@@ -34,5 +41,17 @@ class ShowcaseResource {
 
     }
 
-
+    @GET
+    @Path("/version")
+    @Operation(summary = "Récupère la version logicielle")
+    @APIResponse(
+        responseCode = "200", description = "random", content =
+        [Content(
+            mediaType = MediaType.TEXT_PLAIN,
+            schema = Schema(type = SchemaType.STRING)
+        )]
+    )
+    @Produces(MediaType.TEXT_PLAIN)
+    fun showVersion() = version
+    
 }
