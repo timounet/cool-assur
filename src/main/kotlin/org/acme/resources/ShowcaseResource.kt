c9377835512570b48abae5c3e06f9c9c0e7788a5
PERDRIAU Timothee <timothee.perdriau@allianz.fr>
2021-12-27 19:37:48 +0100

    add other iteration + spellcheck

--- src/main/kotlin/org/acme/resources/ShowcaseResource.kt
+++ src/main/kotlin/org/acme/resources/ShowcaseResource.kt
@@ -14,7 +14,7 @@ import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
-@Tag(name = "z Autres demonstrations", description = "une resource pour montrer d'autres comportements")
+@Tag(name = "z Autres demonstrations", description = "une ressource pour montrer d'autres comportements")
 @Path("/showcase")
 class ShowcaseResource {
 
--- src/main/kotlin/org/acme/resources/UserResource.kt
+++ src/main/kotlin/org/acme/resources/UserResource.kt
@@ -16,7 +16,7 @@ import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
-@Tag(name = "Gestion des utilisateur", description = "une resource pour les utilisateurs de l'application")
+@Tag(name = "Gestion des utilisateur", description = "une ressource pour les utilisateurs de l'application")
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 class UserResource {
